6793a75b1b6e162d1c5b80b0d5ad565482b451a0
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-14 08:34:57 +0530

    [Ganesh] Add:Contact adding is added

---
+++ AddressBook/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Address
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String address { get; set; }
+        public String city { get; set; }
+        public String state { get; set; }
+        public String phoneNumber { get; set; }
+        public String eMail { get; set; }
+
+
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,7 +5,21 @@ namespace AddressBook
     {
         static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to address book!");
+            Address address = new Address();
+            Console.Write("Enter firstName: ");
+            address.firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            address.lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            address.address = Console.ReadLine();
+            Console.Write("Enter city: ");
+            address.city = Console.ReadLine();
+            Console.Write("Enter state: ");
+            address.state = Console.ReadLine();
+            Console.Write("Enter phone umber: ");
+            address.phoneNumber = Console.ReadLine();
+            Console.Write("Enter email: ");
+            address.eMail = Console.ReadLine();
         }
     }
 }
